3f9161db72b98dd7dd6cfea683534db5ff51b7f7
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-05-12 13:12:33 -0700

    feat: add math and table of content plugins

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,16 @@
 import { defineConfig } from "astro/config";
+import rehypeMathjax from "https://esm.sh/rehype-mathjax@7";
+import remarkMath from "https://esm.sh/remark-math@6";
+import remarkToc from "https://esm.sh/remark-math@9";
 
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [
+      remarkMath,
+      remarkToc,
+    ],
+    rehypePlugins: [
+      rehypeMathjax(),
+    ],
+  },
 });
