da5b827d4d9f4d34e2be1c56eb05414ff3fd10dc
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-06-01 10:51:04 -0700

    feat: specify publish criteria in collection definitions

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,21 +2,29 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 import { z } from "astro/zod";
 
+const pattern = ["**/*.md", ...(!import.meta.env.DEV ? ["!**/_*/**"] : [])];
+
 const writings = defineCollection({
-  loader: glob({ base: "./src/content/Writings", pattern: ["**/*.md", "!_*/*.md"] }),
+  loader: glob({ base: "./src/content/Writings", pattern: pattern }),
   schema: z.object({
     title: z.string().optional(),
-    description: z.string(),
-    date: z.coerce.date(),
+    description: z.string().optional(),
+    series: z.string().optional(),
+    date: z.coerce.date().optional(),
     updated: z.coerce.date().optional(),
+    published: z.boolean().default(false),
   }),
 });
 
 const projects = defineCollection({
-  loader: glob({ base: "./src/content/Projects", pattern: "**/*.md" }),
+  loader: glob({
+    base: "./src/content/Projects",
+    pattern: pattern,
+  }),
   schema: z.object({
     title: z.string().optional(),
     date: z.coerce.date().optional(),
+    published: z.boolean().default(false),
   }),
 });
 
