a3c47ddcad49619a028b8b501082ee1e0ad3a94d
ninjack-dev <84806918+ninjack-dev@users.noreply.github.com>
2026-07-20 12:37:02 -0700

    feat: add Atom feed generator

---
+++ src/lib/atom/README.md
@@ -0,0 +1,3 @@
+# Astro Atom
+
+Adds an Atom feed generator to Astro, mirroring the implementation and API of [@astrojs/rss](https://github.com/withastro/astro/tree/main/packages/astro-rss).
---
+++ src/lib/atom/schema.ts
@@ -0,0 +1,39 @@
+import { z } from "astro/zod";
+
+export const atomSchema = z
+	.object({
+		title: z.string().optional(),
+		description: z.string().optional(),
+		pubDate: z
+			.union([z.string(), z.number(), z.date()])
+			.transform((value) => new Date(value))
+			.refine((value) => !isNaN(value.getTime()))
+			.optional(),
+		updatedDate: z
+			.union([z.string(), z.number(), z.date()])
+			.transform((value) => new Date(value))
+			.refine((value) => !isNaN(value.getTime()))
+			.optional(),
+		content: z.string().optional(),
+		link: z.string().optional(),
+		id: z.string(),
+		author: z
+			.union([
+				z.string(),
+				z.object({
+					name: z.string().optional(),
+					email: z.string().optional(),
+					uri: z.string().optional(),
+				}),
+			])
+			.optional(),
+		categories: z.array(z.string()).optional(),
+	})
+	.refine(
+		(data) => data.title || data.description || data.content,
+		{
+			message:
+				"At least one of title, description, or content is required",
+			path: ["title"],
+		},
+	);
